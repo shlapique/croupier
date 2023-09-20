d90a5d63fc7494b3159a79e59001c5111d8ac439
shlapique <gordsdima@gmail.com>
2023-09-20 14:20:00 +0300

    size to human readable, minor fixes

--- croupier.py
+++ croupier.py
@@ -5,18 +5,28 @@ token = TOKEN
 url = 'https://cloud-api.yandex.net/v1/disk/resources'
 
 
+def human_size(nbytes):
+    suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+    i = 0
+    while nbytes >= 1024 and i < len(suffixes)-1:
+        nbytes /= 1024.
+        i += 1
+    f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
+    return '%s %s' % (f, suffixes[i])
+
+
 class Card:
     def __init__(self, name, url, size, date):
         self.name = name
         self.url = url
-        self.size = size
+        self.size = human_size(size)
         self.date = datetime.strptime(
                 date,
                 "%Y-%m-%dT%H:%M:%S%z").strftime("%Y.%m.%d(%H:%M:%S)")
 
 
 def main():
-    print("Choose file(s) to download 🠯: ")
+    print("Choose file(s) to download 🃏: ")
     headers = {'Authorization': 'OAuth ' + token}
     params = {'path': '/kindle'}
     try:
@@ -30,7 +40,7 @@ def main():
 
     # for pretty output
     max_name = max(len(str(x['name'])) for x in items)
-    max_size = max(len(str(x['size'])) for x in items)
+    max_size = max(len(str(human_size(x['size']))) for x in items)
     max_date = max(len(str(x['modified'])) for x in items)
     for item in items:
         card = Card(item['name'],
@@ -40,7 +50,7 @@ def main():
         cards.append(card)
         print(f"[{len(cards)-1}]  {card.name.ljust(max_name)}  {str(card.size).ljust(max_size)}  {card.date.ljust(max_date)}")
 
-    list_to_get = input(f"[enter number(s) (0..{len(cards)-1} | A -- for all)]: ")
+    list_to_get = input(f"[enter number(s) (0..{len(cards)-1}|A)]: ")
     if list_to_get == 'A':
         list_to_get = list(range(len(cards)))
     else:
